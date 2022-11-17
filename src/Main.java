bb168637a8974ed006e6a2a37c6222c8951b336c
q2q2q8 <art-kash@mail.ru>
2022-11-17 14:29:07 +0300

    третье задание сделано

--- src/Main.java
+++ src/Main.java
@@ -2,44 +2,26 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
-    public static int[]  setArrayRandomNumbers() {
-        int [] arr = new int[30];
+    public static int[] setArrayRandomNumbers() {
+        int[] arr = new int[30];
         Random random = new Random();
         for (int i = 0; i < arr.length; i++) {
             arr[i] = random.nextInt(100_000) + 100_000;
         }
         return arr;
     }
-    public static void main(String[] args ) {
-        int[] array = setArrayRandomNumbers ();
-        int max = array[0];
-        int min = array[0];
-        for (int num : array) {
-            if ( num < min) {
-                min = num;
-            }
+
+    public static void main(String[] args) {
+        int[] array = setArrayRandomNumbers();
+        double sum = 0;
+        double days = 30;
+        double average = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum = sum + array[i];
+            average = sum / days;
         }
-        for (int num2 : array ) {
-            if (num2 > max) {
-                max = num2;
-            }
-        }
-        System.out.println("Минимальная сумма трат за день составила " + min + " рублей. " + "Максимальная сумма трат за день составила " + max + " рублей.");
+        System.out.println("Средняя сумма трат за месяц составила " + average + " рублей.");
 
 
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
